e931fd834702f1cad5117912ee8fd50ba5532934
gburlac <gheorghe.burlac@endava.com>
2019-11-10 19:13:38 +0200

    Task 1 done.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime nowFlight = LocalTime.parse("11:15").minusHours(6);
-        System.out.println("Local time now: "+ dateTimeFormatter.format(nowFlight));
+        System.out.println("Local time now: " + dateTimeFormatter.format(nowFlight));
 
     }
 }
