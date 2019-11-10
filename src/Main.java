3729e475e37f6f8c71ef4d58a3f28cac2ed541e6
gburlac <gheorghe.burlac@endava.com>
2019-11-10 21:14:06 +0200

    Start task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 public class Main {
 
@@ -7,7 +11,21 @@ public class Main {
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime nowFlight = LocalTime.parse("11:15").minusHours(6);
-        System.out.println("Local time now: " + dateTimeFormatter.format(nowFlight));
+        System.out.println("What is the duration of the flight? Duration of flight is:  " + dateTimeFormatter.format(nowFlight));
+
+//        Task 2 What day of the week was it released?
+        DayOfWeek dayOfWeek = LocalDate.parse("1977-05-25").getDayOfWeek();
+        System.out.println("What day of the week was it released? Day of release is:  " + dayOfWeek);
+
+//        How many times was it played in the first 2 years?
+        LocalDate start = LocalDate.parse("1977-05-25");
+        LocalDate end = LocalDate.parse("1979-05-25");
+        LocalDate.parse("1977-05-25").plus(2, ChronoUnit.YEARS);
+
+
+
+
+
 
     }
 }
